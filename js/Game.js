dc54664113ee4350ce06068fdfeb92cb4f1633e9
Mary Robin Rose <maryrobinrose@gmail.com>
2019-04-09 17:48:42 -0700

    add pseudo code to handle interaction method

--- js/Game.js
+++ js/Game.js
@@ -44,4 +44,14 @@
     this.activePhrase.addPhraseToDisplay();
   };
 
+  handleInteraction() {
+    /*The clicked/chosen letter must be captured.
+● The clicked letter must be checked against the phrase for a match.
+● If there’s a match, the letter must be displayed on screen instead of the placeholder.
+● If there’s no match, the game must remove a life from the scoreboard.
+● The game should check if the player has won the game by revealing all of the letters in
+the phrase or if the game is lost because the player is out of lives.
+● If the game is won or lost, a message should be displayed on screen.*/
+  }
+
  }
