3bdf2384148caff8ff477f89fc8c8da9a97fe2b8
Mary Robin Rose <maryrobinrose@gmail.com>
2019-04-09 17:36:31 -0700

    add event listener to button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+let game;
+const startButton = document.querySelector('#btn__reset');
+
+//Add click event listener to start button
+startButton.addEventListener('click', () => {
+  //Instantiate a new Game object
+  game = new Game;
+  //Call startGame() method on new Game object
+  game.startGame();
+});
